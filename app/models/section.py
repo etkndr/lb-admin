198346f36457378a88495c9538ec852d88dde0a0
Eric Kinder <etkndr@gmail.com>
2023-12-31 00:07:27 -0500

    section

--- app/models/section.py
+++ app/models/section.py
@@ -2,26 +2,24 @@ from .db import db, environment, SCHEMA
 from sqlalchemy.sql import func
 
 class Instrument(db.Model):
-    __tablename__ = "menus"
+    __tablename__ = "sections"
     
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
-    visible = db.Column(db.Boolean, nullable=False)
+    choice_desc = db.Column(db.String(255))
+    price = db.Column(db.Integer)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
     
-    section = db.relationship("Section", back_populates="menu", cascade="all, delete")
+    menu = db.relationship("Menu", back_populates="section")
     
     def to_dict(self):
         return {
             'id': self.id,
-            "title": self.title,
+            "choice_desc": self.choice_desc,
             "price": self.price,
-            "visible": self.visible,
             "created_at": self.created_at,
             "updated_at": self.updated_at
         }
