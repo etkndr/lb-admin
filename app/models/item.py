a6ecd38111b3ad7e535c4a475be0f541a626db0a
Eric Kinder <etkndr@gmail.com>
2023-12-31 00:11:54 -0500

    item

--- app/models/item.py
+++ app/models/item.py
@@ -2,25 +2,24 @@ from .db import db, environment, SCHEMA
 from sqlalchemy.sql import func
 
 class Instrument(db.Model):
-    __tablename__ = "sections"
+    __tablename__ = "items"
     
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    choice_desc = db.Column(db.String(255))
-    price = db.Column(db.Integer)
+    title = db.Column(db.String(255), nullable=False)
+    desc = db.Column(db.String(255))
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = db.Column(db.DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
     
-    item = db.relationship("Item", back_populates="section", cascade="all, delete")
-    menu = db.relationship("Menu", back_populates="section")
+    section = db.relationship("Section", back_populates="item")
     
     def to_dict(self):
         return {
             'id': self.id,
-            "choice_desc": self.choice_desc,
-            "price": self.price,
+            "title": self.title,
+            "desc": self.desc,
             "created_at": self.created_at,
             "updated_at": self.updated_at
         }
