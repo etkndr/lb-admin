b458862192c2d0fbb797f2e9fbb701e99e09b36c
Eric Kinder <etkndr@gmail.com>
2024-01-25 14:25:10 -0500

    checkbox changing

--- react-app/src/components/MenuBuilder/index.js
+++ react-app/src/components/MenuBuilder/index.js
@@ -1,4 +1,4 @@
-import { useSignal } from "@preact/signals-react"
+import { useSignal, signal, useSignalEffect } from "@preact/signals-react"
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect } from "react"
 import * as menuActions from "../../store/menu"
@@ -9,21 +9,27 @@ export default function MenuBuilder() {
   const dispatch = useDispatch()
   const menus = useSelector((state) => state.menus.menuList)
   const loading = useSignal(true)
+  const menuState = useSignal({})
 
   useEffect(() => {
     dispatch(menuActions.getUserMenus()).then(() => (loading.value = false))
   }, [dispatch])
 
-  function handleVis(menu, vis) {
-    const menuEdit = { ...menu }
-    if (!vis) {
-      menuEdit.visible = "hidden"
+  useEffect(() => {
+    menus?.forEach((menu) => {
+      menuState.value = { ...menuState.value, [menu.id]: menu }
+    })
+  }, [menus])
+
+  function handleVis(menu) {
+    const vis = menuState.value[menu.id].visible
+    if (vis === "visible") {
+      menuState.value[menu.id].visible = "hidden"
     }
-    if (vis) {
-      menuEdit.visible = "visible"
+    if (vis === "hidden") {
+      menuState.value[menu.id].visible = "visible"
     }
-    dispatch(menuActions.editMenuById(menu.id, menuEdit))
-    console.log(menu.visible)
+    dispatch(menuActions.editMenuById(menu.id, menuState.value[menu.id]))
   }
 
   return (
@@ -31,19 +37,23 @@ export default function MenuBuilder() {
       <div className="sidebar-container">
         <h3>MENUS</h3>
         {loading.value && "Loading menus"}
-        {menus?.map((menu, idx) => {
-          return (
-            <li key={idx}>
-              {menu.title}
-              <button onClick={() => (menuId.value = menu.id)}>edit</button>
-              <input
-                type="checkbox"
-                checked={menu.visible === "visible" ? true : false}
-                onChange={(e) => handleVis(menu, e.target.checked)}
-              />
-            </li>
-          )
-        })}
+        {!loading.value &&
+          menus?.map((menu, idx) => {
+            return (
+              <li key={idx}>
+                {menu.title}
+                <button onClick={() => (menuId.value = menu.id)}>edit</button>
+                <input
+                  key={Math.random()}
+                  type="checkbox"
+                  defaultChecked={
+                    menuState.value[menu.id].visible === "visible"
+                  }
+                  onChange={(e) => handleVis(menu)}
+                />
+              </li>
+            )
+          })}
       </div>
       <div>
         <BuildArea />
