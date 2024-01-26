9ec083a43d0013c67105867b13361c40d0e79206
Eric Kinder <etkndr@gmail.com>
2024-01-25 20:12:33 -0500

    render blank if no menu selected

--- react-app/src/components/MenuBuilder/BuildArea.js
+++ react-app/src/components/MenuBuilder/BuildArea.js
@@ -4,32 +4,38 @@ import { useSelector } from "react-redux"
 export default function BuildArea() {
   const menu = useSelector((state) => state.menus.currMenu)
 
+  if (!menu) {
+    return null
+  }
+
   return (
-    <>
-      <h1>{menu?.title}</h1>
-      <p>(${menu?.price}/person)</p>
-      <div>
-        {menu?.sections?.map((section, idx) => {
-          return (
-            <div key={`${idx}1`}>
-              <p>{section.choice_desc}</p>
-              <p>(+${section.price}/person)</p>
-              {section.items?.map((item, idx) => {
-                return (
-                  <div key={`${idx}2`}>
-                    <p>{item.title}</p>
-                    <p>{item.includes}</p>
-                    {item.descs?.map((desc, idx) => {
-                      return <div key={`${idx}3`}>{desc.body}</div>
-                    })}
-                  </div>
-                )
-              })}
-              <h1>. . .</h1>
-            </div>
-          )
-        })}
-      </div>
-    </>
+    menu && (
+      <>
+        <h1>{menu?.title}</h1>
+        <p>(${menu?.price}/person)</p>
+        <div>
+          {menu?.sections?.map((section, idx) => {
+            return (
+              <div key={`${idx}1`}>
+                <p>{section.choice_desc}</p>
+                <p>(+${section.price}/person)</p>
+                {section.items?.map((item, idx) => {
+                  return (
+                    <div key={`${idx}2`}>
+                      <p>{item.title}</p>
+                      <p>{item.includes}</p>
+                      {item.descs?.map((desc, idx) => {
+                        return <div key={`${idx}3`}>{desc.body}</div>
+                      })}
+                    </div>
+                  )
+                })}
+                <h1>. . .</h1>
+              </div>
+            )
+          })}
+        </div>
+      </>
+    )
   )
 }
